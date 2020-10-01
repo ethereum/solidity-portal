8c1be369f1d976db67154163182c4ce112be030a
chriseth <chris@ethereum.org>
2020-10-01 10:50:33 +0200

    Add ACE.

--- compiler.js
+++ compiler.js
@@ -1,15 +1,18 @@
 let worker = new Worker("compiler_worker.js");
-let input = document.getElementById('compilerInput');
+let editor = ace.edit("compilerInput");
 
 let compile = () => {
     worker.postMessage(JSON.stringify({
         language: "Solidity",
-        sources: {"": { content: input.value}},
-        settings: { outputSelection: { "*": {"*": ['evm.bytecode.object', 'evm.gasEstimates']}}}
+        sources: {"": { content: editor.getValue()}},
+        settings: { optimizer: { enabled: true }, outputSelection: { "*": {"*": ['evm.bytecode.object', 'evm.gasEstimates', 'evm.assembly']}}}
     }));
 }
-input.addEventListener('change', compile);
-input.addEventListener('keyup', compile);
+
+editor.session.setMode("ace/mode/javascript");
+editor.getSession().setUseWorker(false);
+editor.renderer.on('afterRender', compile);
+
 worker.addEventListener('message', (message) => {
     if (message.data.version !== undefined) {
         document.getElementById("compiler_version").innerText = message.data.version;
@@ -20,16 +23,17 @@ worker.addEventListener('message', (message) => {
         let errors = document.getElementById('compiler_errors');
         errors.innerHTML = '';
         if (result.errors === undefined) {
-            errors.innerHTML = 'Compilation successful.<br/>';
+            errors.innerHTML = '';
             for (contractName in result.contracts['']) {
                 let contract = result.contracts[''][contractName];
-                errors.innerHTML += '<strong>' + contractName + '</strong><br/>';
-                errors.innerHTML += 'Code size: ' + contract.evm.bytecode.object.length / 2 + ' bytes.<br/>';
+                errors.innerHTML += '<strong>' + contractName + ' (' + contract.evm.bytecode.object.length / 2 + ' bytes)</strong><br/>';
                 errors.innerHTML += 'Deployment costs: ' + contract.evm.gasEstimates.creation.totalCost + ' gas.<br/>';
-                errors.innerHTML += 'Bytecode: ' + contract.evm.bytecode.object.slice(0, 35) + '...<br/>';            }
+                errors.innerHTML += 'Bytecode:<textarea>' + contract.evm.bytecode.object + '</textarea>';
+                errors.innerHTML += 'Assembly:<textarea>' + contract.evm.assembly + '</textarea>';
+            }
         } else {
             for (error of result.errors) {
-                errors.innerHTML += '<pre>' + error.formattedMessage + '</pre>';
+                errors.innerHTML += '<h4>Errors:</h4><pre>' + error.formattedMessage + '</pre>';
             }
         }
     }
